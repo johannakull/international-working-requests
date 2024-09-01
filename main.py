c7b134b2b6dec1df979b529e098eb955fefc1de9
johannakull <johanna.kull@live.de>
2024-09-01 18:12:24 +0100

    calculate request submission deadline based on desired international working start date

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ today = datetime.date.today()
 print("INTERNATIONAL WORKING REQUESTS CALCULATOR\n")
 print(f"Today's date: {today}")
 
-request_deadline = today + datetime.timedelta(days=MIN_REQUEST_PERIOD)
-print(f"Earliest possible international working date (if request submitted today): {request_deadline}")
+earliest_international_working_date = today + datetime.timedelta(days=MIN_REQUEST_PERIOD)
+print(f"Earliest possible international working date (if request submitted today): {earliest_international_working_date}\n")
 
-
+start_date = datetime.datetime.strptime(input("What is the start data of your proposed international working period? "), "%Y-%m-%d").date()
+request_deadline = start_date - datetime.timedelta(days=MIN_REQUEST_PERIOD)
+print(f"The last day to submit the international working request is {request_deadline}.")
