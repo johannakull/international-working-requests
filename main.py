0fe17b4efefcdfa4af7945136f466dd4f7a6f9df
johanna <johanna.kull@live.de>
2024-10-17 16:46:24 +0100

    ability to record new international working periods

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import datetime
 
 MIN_REQUEST_PERIOD_IN_DAYS = 60
@@ -21,6 +22,15 @@ def check_eligibility(max_period):
     pass # TODO: implement this
 
 
+def add_new_period():
+    """Ask for start and end date of the new international working period and add them to data file"""
+
+    start_date = input("Enter the start date of the new international working period (yyyy-mm-dd): ")
+    end_date = input("Enter the end date of the new international working period (yyyy-mm-dd): ")
+
+    with open("international_working_periods.csv", "a", newline="") as records:
+        records.write(f"\n{start_date},{end_date}")
+
 print("INTERNATIONAL WORKING REQUEST CALCULATOR\n")
 print(f"Today's date: {TODAY}")
 
@@ -29,7 +39,7 @@ calculate_earliest_date()
 print(f"""
 1. Calculate request submission deadline based on a desired start date
 2. Check whether a proposed international working period complies with the {MAX_DAYS_IN_YEAR}-day rule
-3. ...
+3. Record a new international working period
 """)
 
 choice = int(input("Please choose one of the options above: "))
@@ -38,5 +48,7 @@ if choice == 1:
     calculate_deadline()
 elif choice == 2:
     check_eligibility(max_period=MAX_DAYS_IN_YEAR)
+elif choice == 3:
+    add_new_period()
 else:
     pass
