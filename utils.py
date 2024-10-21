11bc5995da9a54058c21e46626b61bb56259eab8
johanna <johanna.kull@live.de>
2024-10-21 15:24:21 +0100

    use days instead of time periods to track international working periods

--- utils.py
+++ utils.py
@@ -2,19 +2,35 @@ import csv
 import constants as c
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
-from time_period import TimePeriod
 
+
+def get_dates_in_range(start_date, end_date):
+    """returns a list of dates in a specified range"""
+    dates_in_range = []
+    delta = timedelta(days=1)
+    date = start_date
+    while date <= end_date:
+        dates_in_range.append(date)
+        date += delta
+    return dates_in_range
+
+
+def write_dates_to_file(dates):
+    with open("international_working_days.csv", "a", newline="") as records:
+        for date in dates:
+            records.write(f"{date}\n")
+    print("New date(s) recorded successfully.")
 
 def add_new_period():
     """Ask for start and end date of the new international working period and add them to data file"""
 
     print("\nRecord new international working period\n".upper())
 
-    start_date = input(f"Enter the start date of the new international working period ({c.DATE_FORMAT}): ")
-    end_date = input(f"Enter the end date of the new international working period ({c.DATE_FORMAT}): ")
+    start_date = convert_to_date(input(f"Enter the start date of the new international working period ({c.DATE_FORMAT}): "))
+    end_date = convert_to_date(input(f"Enter the end date of the new international working period ({c.DATE_FORMAT}): "))
+    new_dates = get_dates_in_range(start_date, end_date)
 
-    with open("international_working_periods.csv", "a", newline="") as records:
-        records.write(f"\n{start_date},{end_date}")
+    write_dates_to_file(new_dates)
 
     input("Press enter to continue. ")
     print("\n\n")
@@ -42,52 +58,37 @@ def check_eligibility():
 
     print("\nCheck eligibility of a proposed international working period\n".upper())
 
-    iwps_in_relevant_period = []
-    total_days_in_relevant_period = 0
 
     # 1. PRE-CHECK - check if no of days between (start_date - 12 months) and end_date exceeds allowance
-    # 1.1 get proposed new period from user and add it to list of IWPs in relevant period
-    proposed_new_iwp = TimePeriod()
-    iwps_in_relevant_period.append(proposed_new_iwp)
+    # 1.1 get proposed new period from user and add it to list of days in relevant period
+    start_date = convert_to_date(input(f"Enter a proposed start date ({c.DATE_FORMAT}): "))
+    end_date = convert_to_date(input(f"Enter a proposed end date ({c.DATE_FORMAT}): "))
+    new_dates = get_dates_in_range(start_date, end_date)
+
+    relevant_dates = [date for date in new_dates]
 
     # 1.2 calculate start of relevant period (proposed start_date - 12 months)
-    relevant_period_start_date = proposed_new_iwp.start_date - relativedelta(months=c.LOOKBACK_PERIOD_IN_MONTHS)
+    relevant_period_start_date = relevant_dates[0] - relativedelta(months=-c.LOOKBACK_PERIOD_IN_MONTHS)
 
-    # 1.3 get previously recorded IWPs from data file
-    previously_recorded_iwps = []
-
-    with open("international_working_periods.csv", newline="") as iwp_data:
+    # 1.3 add previously recorded IWPs from data file that fall into relevant period to list of relevant days
+    with open("international_working_days.csv", newline="") as iwp_data:
         time_periods_reader = csv.reader(iwp_data, delimiter=",", quotechar="|")
-        next(time_periods_reader, None)  # skip headers
         for row in time_periods_reader:
-            new_period = tuple(convert_to_date(col) for col in row)
-            previously_recorded_iwps.append(new_period)
+            date = convert_to_date(row[0])
+            if relevant_period_start_date <= date <= end_date:
+                relevant_dates.append(date)
 
-    # 1.4 add previously recorded IWPs within relevant period to list of IWPs in relevant period - BUG: currently
-    # doesn't count periods that start before, but overlap with relevant period start date
-    previously_recorded_iwps_in_relevant_period = [period for period in previously_recorded_iwps
-                                                   if period[0] >= relevant_period_start_date]
-
-    for period in previously_recorded_iwps_in_relevant_period:
-        new_time_period = TimePeriod(start_date=period[0], end_date=period[1])
-        iwps_in_relevant_period.append(new_time_period)
-
-    # 1.5 check if no of days between (start_date - 12 months) and end_date exceeds allowance & output feedback to user
-    for period in iwps_in_relevant_period:
-        days_in_period = period.calculate_no_of_days()
-        total_days_in_relevant_period += days_in_period
-
-    if total_days_in_relevant_period <= c.MAX_DAYS_IN_LOOKBACK_PERIOD:
+    # 1.4 check if no of days between (start_date - 12 months) and end_date exceeds allowance & output feedback to user
+    total_days_in_relevant_period = len(relevant_dates)
+    if total_days_in_relevant_period <= 30:
         print(f"""
-    ✅ Including the proposed international working period, you would have a maximum of 
-    {total_days_in_relevant_period} working days booked within 12 months, which does not exceed the allowance 
-    of {c.MAX_DAYS_IN_LOOKBACK_PERIOD} days.\n
-        """)
+            ✅ Including the proposed international working period, you would have a maximum of 
+            {total_days_in_relevant_period} working days booked within 12 months, which does not exceed the allowance 
+            of {c.MAX_DAYS_IN_LOOKBACK_PERIOD} days.\n
+            """)
         wants_to_record = input("Would you like to add this new international working period to the records? (y/n) ")
         if wants_to_record == 'y':
-            with open("international_working_periods.csv", "a", newline="") as records:
-                records.write(f"\n{proposed_new_iwp.start_date},{proposed_new_iwp.end_date}")
-            print("New international working period recorded successfully.")
+            write_dates_to_file(new_dates)
         else:
             print("The new international working period was not added to the records. Remember to add it once booked.")
     else:
