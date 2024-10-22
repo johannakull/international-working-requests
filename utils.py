3b369f194f9f96080254fd031862c96585a25b75
johanna <johanna.kull@live.de>
2024-10-22 17:00:29 +0100

    add detailed eligibility check for proposed new working period

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def check_eligibility():
     relevant_dates = [date for date in new_dates]
 
     # 1.2 calculate start of relevant period (proposed start_date - 12 months)
-    relevant_period_start_date = relevant_dates[0] - relativedelta(months=-c.LOOKBACK_PERIOD_IN_MONTHS)
+    relevant_period_start_date = relevant_dates[0] - relativedelta(months=c.LOOKBACK_PERIOD_IN_MONTHS)
 
     # 1.3 add previously recorded IWPs from data file that fall into relevant period to list of relevant days
     with open("international_working_days.csv", newline="") as iwp_data:
@@ -82,23 +82,30 @@ def check_eligibility():
     total_days_in_relevant_period = len(relevant_dates)
     if total_days_in_relevant_period <= 30:
         print(f"""
-            ✅ Including the proposed international working period, you would have a maximum of 
-            {total_days_in_relevant_period} working days booked within 12 months, which does not exceed the allowance 
-            of {c.MAX_DAYS_IN_LOOKBACK_PERIOD} days.\n
-            """)
+        ✅ Including the proposed international working period, you would have a maximum of
+        {total_days_in_relevant_period} working days booked within 12 months, which does not exceed the allowance
+        of {c.MAX_DAYS_IN_LOOKBACK_PERIOD} days.\n
+        """)
         wants_to_record = input("Would you like to add this new international working period to the records? (y/n) ")
         if wants_to_record == 'y':
             write_dates_to_file(new_dates)
         else:
             print("The new international working period was not added to the records. Remember to add it once booked.")
     else:
-        print("Further checks needed.")
-
-    # detailed check:
-    # for day in proposed period:
-    #   if [days in [(day-12 months) in datafile + days in new_period[:day]] > 30:
-    #       throw error
-    #   else continue
+        for date in new_dates:
+            lookback_date = date - relativedelta(months=c.LOOKBACK_PERIOD_IN_MONTHS) + timedelta(days=1)
+            dates_in_lookback_period = [day for day in relevant_dates if day >= lookback_date]
+            if len(dates_in_lookback_period) > 30:  # TODO: calculate date proposed period would have to shift to or number of days to cut to be eligible
+                print(f"""
+                ❌ The proposed period exceeds the allowance of {c.MAX_DAYS_IN_LOOKBACK_PERIOD} days
+                in any {c.LOOKBACK_PERIOD_IN_MONTHS} months.
+                """)
+                break
+        else:
+            print(f"""
+            ✅ The proposed period does not exceed the allowance of {c.MAX_DAYS_IN_LOOKBACK_PERIOD} days
+            in any {c.LOOKBACK_PERIOD_IN_MONTHS} months.
+            """)
 
     input("Press enter to continue. ")
     print("\n\n")
